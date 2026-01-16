f7aa724b6bc138e79146e0bfbbfc4a6d48182db1
Vaibhav Gupta <vaibhavgupta6393@gmail.com>
2026-01-16 09:41:49 +0530

    refactor dft4point module to include input variables and initialize cosine/sine values

--- dft4point.v
+++ dft4point.v
@@ -2,6 +2,21 @@
 
 module dft4point ( input wire clk, input wire reset, input wire start, input wire signed [15:0] x0, x1, x2, x3,
                   output reg signed [15:0] Xr [0:3],output reg signed [15:0] Xi [0:3] , output reg done);
-            
+
+input k,n;
+
+reg signed [15:0] x[0:3];
+reg signed [15:0] cosVal[0:3];
+reg signed [15:0] sinVal[0:3];
+reg signed [31:0] tempR, tempI;
+
+always @(*) begin
+  cosVal[0] = 16'sd32767; sinVal[0] = 16'sd0;
+  cosVal[1] = 16'sd0; sinVal[1] = 16'sd32767;
+  cosVal[2] = -16'sd32767; sinVal[2] = 16'sd0;
+  cosVal[3] = 16'sd0; sinVal[3] = -16'sd32767;
+end
+
+
 
 endmodule
