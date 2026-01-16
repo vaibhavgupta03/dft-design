5a24abf5bde5ba8c874478b3079bb02ef939677a
Vaibhav Gupta <vaibhavgupta6393@gmail.com>
2026-01-16 09:37:30 +0530

    add dft4point module for N=4 DFT filter implementation

---
+++ dft4point.v
@@ -0,0 +1,7 @@
+// Designing a dft filter for N=4 by taking the first four points of the DFT matrix
+
+module dft4point ( input wire clk, input wire reset, input wire start, input wire signed [15:0] x0, x1, x2, x3,
+                  output reg signed [15:0] Xr [0:3],output reg signed [15:0] Xi [0:3] , output reg done);
+            
+
+endmodule
